cc33ebf672d66ca070b96edd69a28077e6a32f73
ros_mmerter <ros_mmerter@WROSRM31920.seattleschools.org>
2014-10-30 08:43:56 -0700

    added architecture for characters, edited map to add them(doesn't work yet tho)

--- Slick2D/src/map/Map.java
+++ Slick2D/src/map/Map.java
@@ -1,5 +1,7 @@
 package map;
  
+import java.util.ArrayList;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
@@ -13,7 +15,8 @@ public class Map extends BasicGameState {
     Map  Map;
    // String startinglevel;
     private TiledMap map;
- 
+  //a list of all characters present somewhere on this map
+    private ArrayList<Character> characters;
    // public Map(String name){
    //     this.startinglevel = name;
    // }
@@ -21,7 +24,15 @@ public class Map extends BasicGameState {
     public void init(GameContainer container, StateBasedGame sbg) throws SlickException {
     	// Map = new Map(startinglevel);
     	map = new TiledMap("testdata/levels/level_0.tmx", "testdata/data/img");
+    	characters = new ArrayList<Character>();
+    	
+    	//player = new Player(128,416); <- Issues 
+        //level.addCharacter(player);	<-
   
+    }
+    
+    public void addCharacter(Character c){
+        characters.add(c);
     }
  
     public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException {
@@ -29,7 +40,11 @@ public class Map extends BasicGameState {
     }
  
     public void render(GameContainer container, StateBasedGame sbg, Graphics g) throws SlickException {
-        map.render(0, 0);
+    	map.render(0, 0, 0, 0, 32, 18);
+    	
+    	for(Character c : characters){
+            //c.render(); <- Doesn't work
+        }
     }
  
     //this method is overriden from basicgamestate and will trigger once you press any key on your keyboard
