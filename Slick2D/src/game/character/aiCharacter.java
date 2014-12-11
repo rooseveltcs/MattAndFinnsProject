0f85ab9d1cf7b6e0858ef049d251c72f91e7f231
ros_mmerter <ros_mmerter@WROSRM31920.seattleschools.org>
2014-12-11 08:43:13 -0800

    updated ai getting its direction/waiting

--- Slick2D/src/game/character/aiCharacter.java
+++ Slick2D/src/game/character/aiCharacter.java
@@ -7,29 +7,43 @@ import javax.swing.Timer;
 import org.newdawn.slick.SlickException;
 
 public class aiCharacter extends Character  {
-	
+	//true is right, false is left.
+	protected boolean direction = true;
 	protected boolean isMoving = false;
+	protected int inertia = 65;
 	int pause = 5000;
 	public aiCharacter(float x, float y) throws SlickException {
 		super(x, y);
 		new Timer(pause, checkmove).start();
-		if(isMoving = true){
-			move();
-		}
+		new Timer(pause-3000, checkdirection).start();
+	
 		
 	}
-	public void move() {
-		 
+	public void move(int delta) {
+		 if (isMoving = true){
+			 
+		 }
 	}
 	
     public void movering() {
     	
     	int changeMove = (int) (100*Math.random());
-    	if (changeMove >= 65){
+    	if (changeMove >= inertia){
     		isMoving = !isMoving;
     	}
     }
+    public void direction() {
+    	int changeDirection = (int) (100*Math.random());
+    	if (changeDirection >= inertia/2){
+    		direction = !direction;
+    	}
+    }
     ActionListener checkmove = new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+            movering();
+        }
+    };
+    ActionListener checkdirection = new ActionListener() {
         public void actionPerformed(ActionEvent evt) {
             movering();
         }
