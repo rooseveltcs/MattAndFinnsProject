4a2072f1826da666aec2d68cdfb18cdc88e6b554
ros_mmerter <ros_mmerter@WROSRM31920.seattleschools.org>
2015-01-12 08:04:31 -0800

    working on ai still. Updated map to have an invisible solid roof.

--- Slick2D/src/game/character/aiCharacter.java
+++ Slick2D/src/game/character/aiCharacter.java
@@ -1,9 +1,6 @@
 package game.character;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.lang.Math;
-import javax.swing.Timer;
 import org.newdawn.slick.SlickException;
 
 //seems as though timers aren't being initialized, or are just not working.
@@ -14,52 +11,40 @@ public class aiCharacter extends Character  {
 	public boolean isMoving = true;
 	protected int inertia = 20;
 	//int pause = 5000;
-	public int timer = 0;
 	public aiCharacter(float x, float y) throws SlickException {
 		super(x, y);
 		//new Timer(pause, checkmove).start();
 		//new Timer(pause-3000, checkdirection).start();
-	
-		
+
+
 	}
 	public void move(int delta) { 
 		if (isMoving){
-			timer++;
-			 if(direction){
-				 x = x - (.25f * delta);
-			 }else{
-				 x = x + (.25f * delta);
-			 }
-		 }
-		 if(timer == 25){
-			 changeMove();
-			 timer = 0;
-		 }
+			if(direction){
+				x = x - (.25f * delta);
+			}else{
+				x = x + (.25f * delta);
+			}
+		}
+		int maybeDirection = (int) (100*Math.random());
+		if (maybeDirection >= (inertia/2)) {
+			changeMove();
+		}
 	}
-	
-    public boolean changeMove() {
-    	
-    	int changeMove = (int) (100*Math.random());
-    	if (changeMove >= inertia){
-    		return !isMoving;
-    	}else{
-    		return isMoving;
-    	}    	
-    }
-    public void direction() {
-    	int changeDirection = (int) (100*Math.random());
-    	if (changeDirection >= inertia/2){
-    		direction = !direction;
-    	}
-    }
-    /*ActionListener checkmove = new ActionListener() {
-        public void actionPerformed(ActionEvent evt) {
-            movering();
-        }
-    };
-    ActionListener checkdirection = new ActionListener() {
-        public void actionPerformed(ActionEvent evt) {
-            movering();
-        }
-    };*/
+
+	public boolean changeMove() {
+
+		int changeMove = (int) (100*Math.random());
+		if (changeMove >= inertia){
+			return !isMoving;
+		}else{
+			return isMoving;
+		}    	
+	}
+	public void direction() {
+		int changeDirection = (int) (100*Math.random());
+		if (changeDirection >= inertia/2){
+			direction = !direction;
+		}
+	}
 }
--- Slick2D/src/map/Map.java
+++ Slick2D/src/map/Map.java
@@ -1,5 +1,5 @@
 package map;
- 
+
 import game.character.Character;
 import game.character.JarBro;
 import game.character.Player;
@@ -22,140 +22,140 @@ import tiles.Tile;
 import controller.MouseAndKeyBoardControls;
 
 public class Map extends BasicGameState {
-	
+
 	public Tile[][] tileArray;	
-    //a list of all characters present somewhere on this map
-    private ArrayList<Character> characters;
-    private Player player; 
-    private String levelName;
-    private JarBro jarbro1;
-    private aiCharacter placeHolder;
-    // this is here to receive input.
-    public  MouseAndKeyBoardControls playerControls;
-    protected TiledMap map;
-    private Image background;
-    Music backgroundMusic;
-    
-    public Map(String level) {
-    	levelName = level;
-    	// level should be "hope"
-    	// should use this to reset the window size to fit the map
-    }
-    
-    public void init(GameContainer container, StateBasedGame sbg) throws SlickException {
-    	map = new TiledMap("testdata/levels/" + levelName + ".tmx");
-    	characters = new ArrayList<Character>();
-    	background = new Image("testdata/data/img/backgrounds/" + map.getMapProperty("background", "cabin.png")); 
-    	loadTiledMap();
-    	backgroundMusic = new Music("testdata/Music/jasonMusic.ogg");
-    	backgroundMusic.loop();
-    	jarbro1 = new JarBro(400, 400);
-    	addCharacter(jarbro1);
-        player = new Player(128,400);
-        addCharacter(player);
-        playerControls = new MouseAndKeyBoardControls(player); 
-    }
-    
-    public Tile[][] getTiles(){
-    	return tileArray;
-    }
-    
-    public void loadTiledMap(){
-    	tileArray = new Tile[map.getWidth()][map.getHeight()];  
-    	// gets index layer of the collision layer made in Tiled.
-    	int layerIndex = map.getLayerIndex("CollisionLayer");
-    	for(int x = 0; x < map.getWidth(); x++){
-    		for(int y = 0; y < map.getHeight(); y++){
-    			int tileID = map.getTileId(x, y, layerIndex);
-    			Tile tile= null;
-    			switch(map.getTileProperty(tileID, "tileType", "air")){
-    				case "solid":
-    					tile = new SolidTile(x, y);
-    					break;
-    				default:
-    					tile = new AirTile(x, y);
-    			}
-    			tileArray[x][y] = tile;
-    			// System.out.println(map.getTileProperty(tileID, "tileType", "EMPTY"));
-    		}
-    	}
-    }
-    
-    public void addCharacter(Character c){
-        characters.add(c);
-    }
- 
-    public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException {
-    	if(player.isJumping()){
-    		player.jump(delta);
-    	}
-    	aiCharacter s = placeHolder;
-    	for(int i = 0; i < characters.size(); i++){
-    	if(characters.get(i) instanceof aiCharacter){
+	//a list of all characters present somewhere on this map
+	private ArrayList<Character> characters;
+	private Player player; 
+	private String levelName;
+	private JarBro jarbro1;
+	private aiCharacter placeHolder;
+	// this is here to receive input.
+	public  MouseAndKeyBoardControls playerControls;
+	protected TiledMap map;
+	private Image background;
+	Music backgroundMusic;
+
+	public Map(String level) {
+		levelName = level;
+		// level should be "hope"
+		// should use this to reset the window size to fit the map
+	}
+
+	public void init(GameContainer container, StateBasedGame sbg) throws SlickException {
+		map = new TiledMap("testdata/levels/" + levelName + ".tmx");
+		characters = new ArrayList<Character>();
+		background = new Image("testdata/data/img/backgrounds/" + map.getMapProperty("background", "cabin.png")); 
+		loadTiledMap();
+		backgroundMusic = new Music("testdata/Music/jasonMusic.ogg");
+		backgroundMusic.loop();
+		jarbro1 = new JarBro(400, 400);
+		addCharacter(jarbro1);
+		player = new Player(128,400);
+		addCharacter(player);
+		playerControls = new MouseAndKeyBoardControls(player); 
+	}
+
+	public Tile[][] getTiles(){
+		return tileArray;
+	}
+
+	public void loadTiledMap(){
+		tileArray = new Tile[map.getWidth()][map.getHeight()];  
+		// gets index layer of the collision layer made in Tiled.
+		int layerIndex = map.getLayerIndex("CollisionLayer");
+		for(int x = 0; x < map.getWidth(); x++){
+			for(int y = 0; y < map.getHeight(); y++){
+				int tileID = map.getTileId(x, y, layerIndex);
+				Tile tile= null;
+				switch(map.getTileProperty(tileID, "tileType", "air")){
+				case "solid":
+					tile = new SolidTile(x, y);
+					break;
+				default:
+					tile = new AirTile(x, y);
+				}
+				tileArray[x][y] = tile;
+				// System.out.println(map.getTileProperty(tileID, "tileType", "EMPTY"));
+			}
+		}
+	}
+
+	public void addCharacter(Character c){
+		characters.add(c);
+	}
+
+	public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException {
+		if(player.isJumping()){
+			player.jump(delta);
+		}
+		aiCharacter s = placeHolder;
+		for(int i = 0; i < characters.size(); i++){
+			if(characters.get(i) instanceof aiCharacter){
 				s = (aiCharacter) characters.get(i);
-    			//if(!s.isMoving){
-    				//s.changeMove();
-    			//}else{
-    				s.move(delta);
-    			//}
-    		}
-    	}
-    	playerControls.handleInput(container.getInput(), delta);
-    	player.colliding();
-    }
-    
-    public void render(GameContainer container, StateBasedGame sbg, Graphics g) throws SlickException {
-    	int x_offset = getXOffset();
-        int y_offset = getYOffset();
-        renderBackground();
-    	map.render(-(x_offset%32), -(y_offset%32), x_offset/32, y_offset/32, 32, 18);
-    	for(Character c : characters){
-    		 c.render(x_offset,y_offset);
-        }
-    }
-    
-    public int getXOffset(){
-    	int x_offset = 0;
-    	int halfOfScreen = 500;
-    	int maxOffsetPostition = (int) (map.getWidth()*32)-halfOfScreen;
-    	
-    	if(player.getX() < halfOfScreen){
-    		x_offset = 0;
-    	}else if(player.getX() > maxOffsetPostition){
-    		x_offset = maxOffsetPostition - halfOfScreen;
-    	}else{
-    		x_offset = (int) (player.getX()-halfOfScreen);
-    	}
-    	return x_offset;
-    }
-    
-    public int getYOffset(){
-        int y_offset = 0;
- 
-        int halfHigh = (int) (375);
- 
-        int maxUp = (int) (map.getHeight()*32)-halfHigh;
- 
-        if(player.getY() < halfHigh){
-            y_offset = 0;
-        }else if(player.getY() > maxUp){
-            y_offset = maxUp-halfHigh;
-        }else{
-            y_offset = (int) (player.getY()-halfHigh);
-        }
-        // stops scrolling along the y, so that i can fix jumping first
-        return 0;
-        // return y_offset;
-    }
-    
-    private void renderBackground() {
-    	background.draw();
-    	 
-    }
-    
-    public int getID() {
-        //this is the id for changing states
-        return 1;
-    }
- 
+				if(!s.isMoving){
+					s.changeMove();
+				}else{
+					s.move(delta);
+				}
+			}
+		}
+		playerControls.handleInput(container.getInput(), delta);
+		player.colliding();
+	}
+
+	public void render(GameContainer container, StateBasedGame sbg, Graphics g) throws SlickException {
+		int x_offset = getXOffset();
+		int y_offset = getYOffset();
+		renderBackground();
+		map.render(-(x_offset%32), -(y_offset%32), x_offset/32, y_offset/32, 32, 18);
+		for(Character c : characters){
+			c.render(x_offset,y_offset);
+		}
+	}
+
+	public int getXOffset(){
+		int x_offset = 0;
+		int halfOfScreen = 500;
+		int maxOffsetPostition = (int) (map.getWidth()*32)-halfOfScreen;
+
+		if(player.getX() < halfOfScreen){
+			x_offset = 0;
+		}else if(player.getX() > maxOffsetPostition){
+			x_offset = maxOffsetPostition - halfOfScreen;
+		}else{
+			x_offset = (int) (player.getX()-halfOfScreen);
+		}
+		return x_offset;
+	}
+
+	public int getYOffset(){
+		int y_offset = 0;
+
+		int halfHigh = (int) (375);
+
+		int maxUp = (int) (map.getHeight()*32)-halfHigh;
+
+		if(player.getY() < halfHigh){
+			y_offset = 0;
+		}else if(player.getY() > maxUp){
+			y_offset = maxUp-halfHigh;
+		}else{
+			y_offset = (int) (player.getY()-halfHigh);
+		}
+		// stops scrolling along the y, so that i can fix jumping first
+		return 0;
+		// return y_offset;
+	}
+
+	private void renderBackground() {
+		background.draw();
+
+	}
+
+	public int getID() {
+		//this is the id for changing states
+		return 1;
+	}
+
 }
